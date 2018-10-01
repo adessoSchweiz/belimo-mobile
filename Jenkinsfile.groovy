4f9d6331823622f99c3a51ed665eba1d63ee3f97
josef_sarfraz <josef.sarfraz@adesso.ch>
2018-10-01 17:24:59 +0200

    [BUILD] use appium container

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,8 @@
 @Library('semantic_releasing') _
 
 podTemplate(label: 'mypod', containers: [
-        containerTemplate(name: 'maven', image: 'maven:3.5.2-jdk-8', command: 'cat', ttyEnabled: true)
+        containerTemplate(name: 'maven', image: 'maven:3.5.2-jdk-8', command: 'cat', ttyEnabled: true),
+        containerTemplate(name: 'appium', image: 'appium/appium:1.9.1-p0', command: 'cat', ttyEnabled: true)
 ],
         volumes: [
                 hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock'),
@@ -12,15 +13,13 @@ podTemplate(label: 'mypod', containers: [
             git url: 'https://github.com/adessoSchweiz/belimo-mobile'
         }
 
-        stage('install appium') {
-            sh 'yum install appium -y'
-        }
-
         stage('system tests') {
-            container('maven') {
-                sh "mvn clean integration-test failsafe:integration-test failsafe:verify"
+            container('appium') {
+                container('maven') {
+                    sh "mvn clean integration-test failsafe:integration-test failsafe:verify"
+                }
+                junit allowEmptyResults: true, testResults: '**/target/failsafe-reports/TEST-*.xml'
             }
-            junit allowEmptyResults: true, testResults: '**/target/failsafe-reports/TEST-*.xml'
         }
     }
 }
