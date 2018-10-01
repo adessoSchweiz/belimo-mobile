cfb183b2abe9e8136adcfa4fb619ce4ed9242ebe
josef_sarfraz <josef.sarfraz@adesso.ch>
2018-10-01 17:20:38 +0200

    [BUG] added maven container in build pod

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,9 +1,7 @@
 @Library('semantic_releasing') _
 
 podTemplate(label: 'mypod', containers: [
-        containerTemplate(name: 'docker', image: 'docker', ttyEnabled: true, command: 'cat'),
-        containerTemplate(name: 'kubectl', image: 'lachlanevenson/k8s-kubectl:v1.11.2', command: 'cat', ttyEnabled: true),
-        containerTemplate(name: 'npm-jdk', image: 'khinkali/npm-java:0.0.4', ttyEnabled: true, command: 'cat'),
+        containerTemplate(name: 'maven', image: 'maven:3.5.2-jdk-8', command: 'cat', ttyEnabled: true)
 ],
         volumes: [
                 hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock'),
